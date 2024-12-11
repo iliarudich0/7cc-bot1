c4da732d4ddf1a0833f4c6a9646e2e18cc4caede
iliarudich0 <iliarudich3mz@gmail.com>
2024-12-11 15:25:35 +0100

    Update 7cc-bot.py

--- 7cc-bot.py
+++ 7cc-bot.py
@@ -34,7 +34,7 @@ questions = {
 }
 
 # Основная логика вашего бота должна быть здесь
-if __name__ == "__main__":
+
    if __name__ == "__main__":
     logging.info("Bot started. Configuring handlers and starting polling.")
     
